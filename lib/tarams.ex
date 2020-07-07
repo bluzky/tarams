47378fbefe85d0ca7c4976037e414942abdc4487
DN.Wangchuk <dung.nt@yeah1.vn>
2020-07-07 17:57:55 +0700

    doc: update document and document structure

--- lib/tarams.ex
+++ lib/tarams.ex
@@ -1,10 +1,69 @@
 defmodule Tarams do
-  @moduledocs """
+  @moduledoc """
+  Function for parsing and validating input params with predefined schema
+
+  **Basic usage**
+  ```elixir
+    @index_params_schema  %{
+      keyword: :string,
+      status: [type: string, required: true],
+      group_id: [type: :integer, validate: {:number, [greater_than: 0]}]
+    }
+
+    def index(conn, params) do
+      with {:ok, better_params} <- Tarams.parse(@index_params_schema, params) do
+        # do anything with your params
+      else
+        {:error, changset} -> # return params error
+      end
+    end
+
+  ```
+
+  A Schema is a simple map of `field: options`
+
+  ### Sample schema
+  ```elixir
+    schema1 = %{
+      keyword: :string
+      status: [type: :string, required: true, validate: {:inclusion, ["open", "processing", "completed"]}],
+      page: [type: :integer, default: 1],
+      start_date: [type: :date, default: &Timex.today/0]
+    }
+  ```
+
+  ### Field options
+
+  - If there is no options, a field can be written in short form `<field_name>: <type>`.
+  - `required` is false by default, this field is used to check if a field is required or not
+  - `validate` define how a field is validated. You can use any validation which `Ecto.Changeset` supports. There is a simple rule to map schema declaration with `Ecto.Changeset` validation function.
+  Simply concatenate `validate` and validation type to get `Ecto.Changeset` validation function.
+
+  ### Example
+  ```elixir
+    %{status: [type: string, validate: {:inclusion, ["open", "pending"]}]}
+
+    # is translated to
+    Ecto.Changeset.validate_inclustion(changeset, :status, ["open", "pending"])
+  ```
+
+  - `default`: set default value. It could be a value or a function, if is is a function, it will be evaluated each time `parse` function is called.
+
+  ### Example
+  ```elixir
+    %{
+      category: [type: :string, default: "elixir"],
+      end_date: [type: :date, default: &Timex.today/1]
+    }
+  ```
 
   """
 
   import Ecto.Changeset
 
+  @doc """
+  Parse and validate input params with predefined schema
+  """
   @spec parse(map, map) :: {:ok, map} | {:error, Ecto.Changeset.t()}
   def parse(schema, params) do
     default = get_default(schema)
--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule Tarams.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      name: "Tarams",
+      description: description(),
+      source_url: "https://github.com/bluzky/tarams",
+      docs: docs()
     ]
   end
 
@@ -18,12 +23,30 @@ defmodule Tarams.MixProject do
     ]
   end
 
+  defp package() do
+    [
+      maintainers: ["Dung Nguyen"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/bluzky/tarams"}
+    ]
+  end
+
+  defp description() do
+    "Simplest library to parse and validate parameters in elixir "
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"]
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ecto, "~> 3.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ecto, "~> 3.0"},
+      {:ex_doc, "~> 0.22.1"}
     ]
   end
 end
